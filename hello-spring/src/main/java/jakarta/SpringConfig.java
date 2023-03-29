617793172a48a65ec8e0bddf0c33d6adcee9c7c6
JongchanJeon <tristan6349@naver.com>
2023-03-30 02:03:17 +0900

    Spring add

---
+++ hello-spring/src/main/java/jakarta/SpringConfig.java
@@ -0,0 +1,21 @@
+package jakarta;
+
+import jakarta.repository.MemberRepository;
+import jakarta.repository.MemoryMemberRepository;
+import jakarta.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
