6196a8104babd13d79ba51dbade34fe46543a4b5
Jongchan Jeon <tristan6349@naver.com>
2023-05-29 22:13:20 +0900

    10장 추가

--- basic_spring/spring_study_security10/src/main/java/com/kong/king/spring/security10/config/SecurityConfig.java
+++ basic_spring/spring_study_security10/src/main/java/com/kong/king/spring/security10/config/SecurityConfig.java
@@ -19,19 +19,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 	}
 	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("user1")
-		.password("$2a$10$O8a6d9fbzTsFETLbI0UVTuyvkFoxCrkqTPm5/24jbI6m7uqXFGrZm")
-		.roles("USER");
-	}
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.inMemoryAuthentication().withUser("user1")
+//		.password("$2a$10$O8a6d9fbzTsFETLbI0UVTuyvkFoxCrkqTPm5/24jbI6m7uqXFGrZm")
+//		.roles("USER");
+//	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+//		http.csrf().disable();
 		http.authorizeRequests()
 			.antMatchers("/sample/all").permitAll()
-			.antMatchers("/sample/member").hasRole("USER");
-		http.formLogin().loginPage("/login").defaultSuccessUrl("/sample/all");
+			.antMatchers("/sample/member").hasRole("USER")
+			.antMatchers("/guest/**").permitAll()
+			.antMatchers("/manager/**").hasRole("MANAGER")
+			.antMatchers("/admin/**").hasRole("ADMIN");
+		http.formLogin().defaultSuccessUrl("/sample/all");
+//		http.formLogin().loginPage("/login").defaultSuccessUrl("/sample/all");
+//		http.logout().logoutUrl("/logout").invalidateHttpSession(true);
 //		http.formLogin();
 	}
 	
